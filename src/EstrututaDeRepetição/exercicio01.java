900e541ca2c2156726d81ff4b2c5ad15bc3a7185
maicon8850 <maicon.douglas3527@gmail.com>
2024-07-31 13:57:57 -0300

    Adição de mais dois exercicio

---
+++ src/EstrututaDeRepetição/exercicio01.java
@@ -0,0 +1,20 @@
+package EstrututaDeRepetição;
+
+import java.util.Scanner;
+
+public class exercicio01 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in); // fazer a leitura dos dados
+
+        int x = sc.nextInt();
+        int soma = sc.nextInt();
+        while (x != 0) {
+            ( )
+            x = sc.nextInt();
+
+        }
+   sc.close();
+
+    }
+
+}
