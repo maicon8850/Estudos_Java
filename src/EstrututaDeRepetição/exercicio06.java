e52b2516351c1d2730a68ff5422cd01c90d8b717
maicon8850 <maicon.douglas3527@gmail.com>
2024-08-01 13:16:33 -0300

    Segundo exercicio usando o  for

--- src/EstrututaDeRepetição/exercicio06.java
+++ src/EstrututaDeRepetição/exercicio06.java
@@ -1,19 +1,32 @@
-package EstrututaDeRepetição;
-
 import java.util.Scanner;
 
-
 public class exercicio06 {
+//
+//Leia um valor inteiro N. Este valor será a quantidade de valores inteiros X que serão lidos em seguida.
+//Mostre quantos destes valores X estão dentro do intervalo [10,20] e quantos estão fora do intervalo, mostrando
+//essas informações conforme exemplo (use a palavra "in" para dentro do intervalo, e "out" para fora do intervalo).
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        int n = sc.nextInt();
 
-        for ( int i=1; i >= 10 ; i++) {
+        int in = 0;
+        int out = 0;
 
-            int produto = i * N;
-            System.out.println(i + "x" + N + "=" + produto);
+        for (int i=0; i<n; i++) {
+            int x = sc.nextInt();
+            if (x >= 10 && x <= 20) {
+                in = in + 1;
+            }
+            else {
+                out = out + 1;
+            }
         }
-     sc.close();
+
+        System.out.println(in + " in");
+        System.out.println(out + " out");
+
+        sc.close();
     }
 }
